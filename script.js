1434e5e75c43bebf8a0429668a7d31d9becf2995
Yuvraj Chaudhary <yuvr4aj@gmail.com>
2024-02-09 00:30:30 +0530

    push

---
+++ script.js
@@ -0,0 +1,23 @@
+gsap.from("#img3",{
+    delay:0.4,
+    y:-70,
+    opacity:0,
+    duration:1
+})
+gsap.from("#img1",{
+    delay:0.4,
+    y:70,
+    opacity:0,
+    duration:1
+})
+gsap.from("#img2",{
+    delay:0.4,
+    x:70,
+    opacity:0,
+    duration:1
+})
+gsap.from("#main>h1",{
+    delay:0.4,
+    opacity:0,
+    duration:1
+})
